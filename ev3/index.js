73d0bd7faa2b705c2a124dc2f4a72fda02e5bcdf
wondger <wondger@gmail.com>
2014-04-11 20:52:55 +0800

    ev3 module init

---
+++ ev3/index.js
@@ -0,0 +1,94 @@
+/*!
+ * # ev3 api
+ * @author yanmu.wj@taobao.com
+ * @date 2014-04-11
+ */
+
+/*!*/
+var EV3Robot = require('./ev3-node/EV3Robot.js');
+var robot = new EV3Robot.Robot();
+
+/**
+ * move2left
+ *
+ * 向左移动
+ *
+ * @param {Numbe} speed
+ * @return
+ */
+exports.move2left = function(speed) {
+  
+};
+
+/**
+ * move2right
+ *
+ * 向右移动
+ *
+ * @param speed
+ * @return
+ */
+exports.move2right = function(speed) {
+  
+};
+
+/**
+ * move2forward
+ *
+ * 向前移动
+ *
+ * @param {Numbe} speed
+ * @return
+ */
+exports.move2forward = function(speed) {
+  
+};
+
+/**
+ * move2back
+ *
+ * 向后移动
+ *
+ * @param {Numbe} speed
+ * @return
+ */
+exports.move2back = function(speed) {
+  
+};
+
+exports.stopMove = function() {
+  
+};
+
+/**
+ * playSound
+ *
+ * 播放音频
+ *
+ * @param {Sring} sound
+ * @param {Number} volumn
+ * @param {Number} duration
+ * @return
+ */
+exports.playSound = function(sound, volumn, duration) {
+  
+};
+
+exports.stopMove = function() {
+  
+};
+
+exports.stopAll = function() {
+  
+};
+
+/**
+ * display
+ *
+ * 屏幕显示
+ *
+ * @return
+ */
+exports.display = function() {
+  
+};
