85afa71d9aea8f4e1ce1eb86bcfef6940c16a369
wondger <wondger@gmail.com>
2014-05-07 20:15:11 +0800

    update

--- socket.js
+++ socket.js
@@ -15,24 +15,31 @@ module.exports = function(server) {
     console.log("socket.io connected...");
 
     socket.on("forward", function(data) {
+      console.log("forward");
     });
 
     socket.on("back", function(data) {
+      console.log("back");
     });
 
     socket.on("left", function(data) {
+      console.log("left");
     });
 
     socket.on("right", function(data) {
+      console.log("right");
     });
 
     socket.on("faster", function(data) {
+      console.log("faster");
     });
 
     socket.on("slower", function(data) {
+      console.log("slower");
     });
 
     socket.on("stop", function(data) {
+      console.log("stop");
     });
   });
 };
